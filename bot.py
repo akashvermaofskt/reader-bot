77ab5db182ab58e194cef993e307447c0c90da1b
Akash Verma <akash.verma@peritus.ai>
2021-07-13 01:36:14 +0530

    Added code to pull and send images to discord channel

--- bot.py
+++ bot.py
@@ -1,14 +1,50 @@
 import os
+import time
+from datetime import date
 
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
+GUILD = os.getenv("DISCORD_SERVER")
 READING_CHANNEL_ID = int(os.getenv("DISCORD_READING_CHANNEL"))
 
 client = discord.Client()
+dir_name = "Books/Drive"
+
+
+def get_offset_index():
+    list_of_files = list(
+        filter(
+            lambda x: os.path.isfile(os.path.join("Books/Drive/OLD/", x)),
+            os.listdir("Books/Drive/OLD/"),
+        )
+    )
+    return len(list_of_files)
+
+
+def get_all_ss_to_post():
+    list_of_files = filter(
+        lambda x: os.path.isfile(os.path.join(dir_name, x))
+        and x[0] != "."
+        and len(x) > 4
+        and x[-4:] == ".png",
+        os.listdir(dir_name),
+    )
+    # Sort list of files based on last modification time in ascending order
+    list_of_files = sorted(
+        list_of_files, key=lambda x: os.path.getmtime(os.path.join(dir_name, x))
+    )
+
+    for file_name in list_of_files:
+        file_path = os.path.join(dir_name, file_name)
+        timestamp_str = time.strftime(
+            "%m/%d/%Y :: %H:%M:%S", time.gmtime(os.path.getmtime(file_path))
+        )
+        print(timestamp_str, " -->", file_name)
+
+    return list_of_files
 
 
 @client.event
@@ -16,18 +52,32 @@ async def on_ready():
     print(client.guilds)
     for guild in client.guilds:
         if guild.name == GUILD:
+            print(
+                f"{client.user} is connected to the following guild:\n"
+                f"{guild.name}(id: {guild.id})"
+            )
             break
-
-    print(
-        f"{client.user} is connected to the following guild:\n"
-        f"{guild.name}(id: {guild.id})"
-    )
 
     channel = client.get_channel(READING_CHANNEL_ID)
 
-    sent_message = await channel.send("hello !!!")
-    emoji = "\N{THUMBS UP SIGN}"
-    await sent_message.add_reaction(emoji)
+    # sent_message = await channel.send("hello !!!")
+
+    # await sent_message.add_reaction("⭐")
+
+    # sent_message = await channel.send(file=discord.File("static/icon.jpeg"))
+    # await sent_message.add_reaction("⭐")
+
+    today = date.today().strftime("%d/%m/%Y")
+    await channel.send("###")
+    await channel.send(f"""`{today}`""")
+    await channel.send("###")
+    offset_index = get_offset_index()
+    files = get_all_ss_to_post()
+    for idx, file_name in enumerate(files):
+        await channel.send(f"Page no. {offset_index+idx}")
+        sent_message = await channel.send(file=discord.File(dir_name + "/" + file_name))
+        await sent_message.add_reaction("⭐")
+        await channel.send("---------------")
 
 
 client.run(TOKEN)
