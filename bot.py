9a3c53cab1b5c248de5ff719602cb264cbf3fb7a
Akash Verma <akash.verma@peritus.ai>
2021-08-08 02:17:34 +0530

    Added page group based posting (based on Reading Speed and Reading Time

--- bot.py
+++ bot.py
@@ -6,90 +6,113 @@ from dateutil import parser
 import discord
 from dotenv import load_dotenv
 from localStoragePy import localStoragePy
-
-localStorage = localStoragePy("reader_bot", "json")
-current_time = datetime.now()
-last_run = parser.parse(localStorage.getItem("last_run"))
-seconds_passed_since_last_run = (current_time - last_run).total_seconds()
-print("\n\n######")
-print(f"Time now: {str(current_time)}")
-print(f"Last Ran: {str(last_run)}")
-print(f"Seconds passed since last run: {seconds_passed_since_last_run}")
-
-if seconds_passed_since_last_run < 86400:
-    print("24 hours didn't pass, quiting!")
-    quit()
-else:
-    print("STARTING BOT!")
+from pdf_utils import get_page_groups
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_SERVER")
 READING_CHANNEL_ID = int(os.getenv("DISCORD_READING_CHANNEL"))
-POSTED = False
+BOOK_NAME = os.getenv("BOOK_NAME")
+BOOK_START_PAGE = int(os.getenv("BOOK_START_PAGE"))
+READING_SPEED_WPM = int(os.getenv("READING_SPEED_WPM"))
+READING_TIME_IN_MINS = int(os.getenv("READING_TIME_IN_MINS"))
+MASTER_FLAG = False
+
 
 client = discord.Client()
-dir_name = "Books/Drive/PENDING"
+localStorage = localStoragePy("reader_bot", "json")
+
+
+def ensure_run():
+    current_time = datetime.now()
+    last_run = parser.parse(localStorage.getItem("last_run"))
+    seconds_passed_since_last_run = (current_time - last_run).total_seconds()
+    print("\n\n######")
+    print(f"Time now: {str(current_time)}")
+    print(f"Last Ran: {str(last_run)}")
+    print(f"Seconds passed since last run: {seconds_passed_since_last_run}")
+
+    if seconds_passed_since_last_run < 86400:
+        return False
+    else:
+        return True
 
 
 def get_offset_index():
     list_of_files = list(
         filter(
-            lambda x: os.path.isfile(os.path.join("Books/Drive/DONE/", x)),
-            os.listdir("Books/Drive/DONE/"),
+            lambda x: os.path.isfile(os.path.join(f"Books/{BOOK_NAME}/DONE/", x)),
+            os.listdir(f"Books/{BOOK_NAME}/DONE/"),
         )
     )
     return len(list_of_files)
 
 
-def get_all_ss_to_post():
+def get_all_ss_to_post_time_based_fixed():
+    ss_dir_name = f"Books/{BOOK_NAME}/PENDING"
     list_of_files = filter(
-        lambda x: os.path.isfile(os.path.join(dir_name, x))
+        lambda x: os.path.isfile(os.path.join(ss_dir_name, x))
         and x[0] != "."
         and len(x) > 4
         and x[-4:] == ".jpg",
-        os.listdir(dir_name),
+        os.listdir(ss_dir_name),
     )
     # Sort list of files based on last modification time in ascending order
     list_of_files = sorted(
-        list_of_files, key=lambda x: os.path.getmtime(os.path.join(dir_name, x))
+        list_of_files, key=lambda x: os.path.getmtime(os.path.join(ss_dir_name, x))
     )
 
     for file_name in list_of_files:
-        file_path = os.path.join(dir_name, file_name)
+        file_path = os.path.join(ss_dir_name, file_name)
         timestamp_str = time.strftime(
             "%m/%d/%Y :: %H:%M:%S", time.gmtime(os.path.getmtime(file_path))
         )
         print(timestamp_str, " -->", file_name)
 
-    return list_of_files
+    return list_of_files[:5]
+
+
+def get_all_ss_to_post_page_group_based():
+    ss_dir_name = f"Books/{BOOK_NAME}/PENDING"
+    list_of_files = filter(
+        lambda x: os.path.isfile(os.path.join(ss_dir_name, x))
+        and x[0] != "."
+        and len(x) > 4
+        and x[-4:] == ".jpg",
+        os.listdir(ss_dir_name),
+    )
+    # Sort list of files based on last modification time in ascending order
+    list_of_files = sorted(
+        list_of_files, key=lambda x: os.path.getmtime(os.path.join(ss_dir_name, x))
+    )
+    page_groups = get_page_groups(
+        BOOK_NAME, BOOK_START_PAGE, READING_SPEED_WPM, READING_TIME_IN_MINS
+    )
+
+    starting_page = int(list_of_files[0].split(".")[0])
+    current_page_group = []
+    for page_group in page_groups:
+        if starting_page in page_group:
+            current_page_group.extend(page_group)
+            break
+
+    return [
+        file_name
+        for file_name in list_of_files
+        if int(file_name.split(".")[0]) in current_page_group
+    ]
 
 
 @client.event
 async def on_ready():
-    if POSTED:
-        print("Already posted for today, quiting!")
-        quit()
-
-    print(client.guilds)
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            print(
-                f"{client.user} is connected to the following guild:\n"
-                f"{guild.name}(id: {guild.id})"
-            )
-            break
+    if MASTER_FLAG is False:
+        raise "Master Flag is False, quiting!"
 
     channel = client.get_channel(READING_CHANNEL_ID)
+    ss_dir_name = f"Books/{BOOK_NAME}/PENDING"
 
-    # sent_message = await channel.send("hello !!!")
-
-    # await sent_message.add_reaction("⭐")
-
-    # sent_message = await channel.send(file=discord.File("static/icon.jpeg"))
-    # await sent_message.add_reaction("⭐")
     print("DONE Pages -> ", get_offset_index())
-    files = get_all_ss_to_post()
+    files = get_all_ss_to_post_page_group_based()
     if len(files) != 0:
         print("Posting Begins!")
         today = date.today().strftime("%d/%m/%Y")
@@ -97,22 +120,33 @@ async def on_ready():
         await channel.send(f"""`{today}`""")
         await channel.send("###")
         offset_index = get_offset_index()
-        for idx, file_name in enumerate(files[:5]):
+        for idx, file_name in enumerate(files):
             await channel.send(f"Page no. {offset_index+idx}")
             sent_message = await channel.send(
-                file=discord.File(dir_name + "/" + file_name)
+                file=discord.File(ss_dir_name + "/" + file_name)
             )
             await sent_message.add_reaction("⭐")
             await channel.send("---------------")
             print(f"Posted {file_name}")
-            os.rename(dir_name + "/" + file_name, "Books/Drive/DONE/" + file_name)
+            os.rename(
+                ss_dir_name + "/" + file_name, f"Books/{BOOK_NAME}/DONE/" + file_name
+            )
             print(f"Moved {file_name}")
+        await channel.send("@Reader")
     else:
         print("Nothing to post")
 
     localStorage.setItem("last_run", str(datetime.now()))
-    print("All Done, quiting!")
-    quit()
+    raise "All Done, quiting!"
 
 
-client.run(TOKEN)
+def main():
+    bot_run_pending = ensure_run()
+    if bot_run_pending:
+        client.run(TOKEN)
+    else:
+        raise "Bot not supposed to run"
+
+
+if __name__ == "__main__":
+    main()
--- pdf_utils.py
+++ pdf_utils.py
@@ -1,10 +1,51 @@
+import os
 from pdf2image import convert_from_path
+import PyPDF2
+from dotenv import load_dotenv
 
-pages = convert_from_path("Books/Drive/drive.pdf", 200)
 
-page_no = 71
-for page in pages[80:]:
-    print(f"Saving Page no. {page_no}")
-    page.save(f"Books/Drive/PENDING/{page_no}-out.jpg", "JPEG")
-    print(f"Done Page no. {page_no}")
-    page_no = page_no + 1
+def pdf_to_images(book_name, book_start_page):
+    pages = convert_from_path(f"Books/{book_name}/{book_name}.pdf", 400)
+    for page_no, page in enumerate(pages[book_start_page:]):
+        print(f"Saving Page no. {page_no}")
+        page.save(f"Books/{book_name}/PENDING/{page_no}.jpg", "JPEG")
+        print(f"Done Page no. {page_no}")
+
+
+def get_page_groups(
+    book_name, book_start_page, reading_speed_wpm, reading_time_in_mins
+):
+    pdf = open(f"Books/{book_name}/{book_name}.pdf", "rb")
+    pdf_reader = PyPDF2.PdfFileReader(pdf)
+    print(" No. Of Pages :", pdf_reader.numPages)
+    page_groups = []
+    page_group = []
+    count = 0
+    for i in range(book_start_page, pdf_reader.numPages):
+        page = pdf_reader.getPage(i)
+        words = page.extractText().split()
+        print(f"Page no. {(i-book_start_page)} has {len(words)}")
+        count = count + len(words)
+        page_group.append(i - book_start_page)
+        if count >= (reading_speed_wpm * reading_time_in_mins):
+            page_groups.append(page_group)
+            page_group = []
+            count = 0
+
+    if len(page_group) > 0:
+        page_groups.append(page_group)
+        page_group = []
+        count = 0
+
+    print(page_groups)
+    print(len(page_groups))
+    return page_groups
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    BOOK_NAME = os.getenv("BOOK_NAME")
+    BOOK_START_PAGE = int(os.getenv("BOOK_START_PAGE"))
+    READING_SPEED_WPM = int(os.getenv("READING_SPEED_WPM"))
+    READING_TIME_IN_MINS = int(os.getenv("READING_TIME_IN_MINS"))
+    get_page_groups(BOOK_NAME, BOOK_START_PAGE, READING_SPEED_WPM, READING_TIME_IN_MINS)
